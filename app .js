bc6b797ca84b5c46ba6eb033aaf7fbada3439825
fahadarif1 <134074010+fahadarif1@users.noreply.github.com>
2023-05-29 19:09:44 +0500

    Add files via upload

---
+++ app .js
@@ -0,0 +1,95 @@
+// // chapter 1
+
+// alert("First Name");
+// alert("Last Name");
+// alert("Email");
+// alert("Phone Number");
+// alert("Password");
+
+// alert("You're learning JavaScript!");
+
+// alert("Hello ! Jawan Pakstan")
+
+// // chapter 2
+
+// myVariable = 10
+
+// myVariable
+// myVariable = "Hello, world!"
+
+// var teamName;
+// teamName = "OpenAI Team";
+// alert(teamName);
+
+// var bestMan = "Charlie";
+// bestMan = "John";
+
+
+// // chapter 3
+
+
+// var caseQty;
+
+// var caseQty;
+// caseQty = 144;
+
+// var num = parseInt("9");
+
+// var sumResult;
+// sumResult = 5 + 7;
+
+
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+
+// var myNumber = 5;
+// myNumber = myNumber + 3;
+
+// // chapter 4
+
+// var productCost = 3.45;
+
+// var nameofband;
+
+// var myNumber = 42;
+
+
+// var firstName = "Chat";
+// var lastName = "GPT";
+// var fullName = firstName + lastName;
+
+
+
+
+// // chapter 5
+
+// var remainder = 10 % 3;
+
+// var largeNum = 1000 * 2000;
+
+// var num1 = 10;
+// var num2 = 5;
+// var result = num1 - num2;
+
+// chapter  6
+
+// var num 10;
+// var newNum = num--;
+
+// var num1 = 10;
+// var num2 = ++num1;
+
+// var num = 5;
+// Number ++;
+// alert("the value is :"+ number)
+
+// chapter 7
+
+// var calculatedNum = (2 + 2) * (4 + 2);
+// console.log(calculatedNum);
+
+// var calculatedNum = (2 + 2) * 4) + 2;
+// console.log(calculatedNum);
+
+
